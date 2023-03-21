97cdaa5c5384cb1d9ecccda2561a1f8f4ab69d98
mozyakov <s.mozyakov@gmail.com>
2023-03-21 17:11:14 +0700

    Update Main.java
    Изменил возраст на 21 год, иначе все были младше 18.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         List<Person> person = new ArrayList<>(Arrays.asList(
                 new Person("Petr", "la-Petrov", 16),
-                new Person("Albert", "Van-Der-fon-Gor", 15),
+                new Person("Albert", "Van-Der-fon-Gor", 21),
                 new Person("Kir", "Der-fon-Persi", 15),
                 new Person("Ivan", "la-Ivanov", 15)
         ));
