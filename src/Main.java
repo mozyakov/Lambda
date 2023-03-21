ee2f131c5a0c1321f43f26a87c2ecfd4548033c9
mozyakov <s.mozyakov@gmail.com>
2023-03-21 16:45:00 +0700

    удалил класс, добавил лямбду.
    Удалил класс компаратора, в методе main заменил объект этого удалённого класса на лямбду с той же логикой.

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,18 @@ public class Main {
                 new Person("Ivan", "la-Ivanov", 15)
         ));
 
-        Collections.sort(person, new PersonComparator());
+        Collections.sort(person, (p1, p2) ->{
+            int len1 = p1.getSurname().split("-").length;
+            int len2 = p2.getSurname().split("-").length;
+            if(len1 != len2) {
+                return Integer.compare(len2, len1); //сначала большее len, большее знатнее
+            }
+            return Integer.compare(p2.getAge(), p1.getAge()); //сначала  большее p, большее знатнее
+        });
+
         for (Person person1 : person) {  //вывод отсортированного списка
             System.out.println(person1);
         }
-        System.out.println(person); //ещё раз вывожу отсортированный список
+
     }
 }
